784f6108f920f043f6a698768116f44bf31be3c1
Vladimir <105167409+Tsaranchik@users.noreply.github.com>
2023-05-14 22:20:06 +0400

    MLiDM: Import lab №3

---
+++ ml3/scripts/main.js
@@ -0,0 +1,41 @@
+// /**
+//  * Валидация параметров ввода
+//  * @param masOfMas
+//  */
+// function validateElement(mas) {
+//     return;
+// }
+
+/**
+ * Проверка, являются ли отношения функцией
+ * @param masOfMas
+ */
+function check(mas) {
+    let res = []; // результирующий массив
+    let isFunction = true; // флаг
+    for (let i = 0; i < mas.length; ++i) {
+        let elem = mas[i].split(' '); // сплитим пару элементов массива [1 1]
+        if (res.includes(elem[0])) {
+            isFunction = false; // проверка на наличие элемента в результирующем массиве
+            break;
+        }
+        res[res.length] = elem[0]; // добавление элемента
+    }
+
+    return isFunction;
+}
+
+/**
+ * Основная функция
+ */
+function main() {
+    let mas = document.getElementById('id_mas').value;
+    document.getElementById('result').innerHTML = 'Результат: <br>';
+    mas = mas.split(', ');
+    let IsFunction = true;
+
+    IsFunction = check(mas);
+    if (!IsFunction)
+        document.getElementById('result').innerHTML += 'Отношения не являются функцией <br>';
+    else document.getElementById('result').innerHTML += 'Отношения являются функцией <br>';
+}
